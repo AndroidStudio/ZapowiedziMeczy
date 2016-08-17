73a75210e97a7d151613a988b4fc415032bffd58
Przemek <przemek33665@gmail.com>
2016-08-17 14:31:16 +0200

    update

--- MeczykiModule/src/main/java/com/footballscore/activities/BaseActivity.java
+++ MeczykiModule/src/main/java/com/footballscore/activities/BaseActivity.java
@@ -3,14 +3,11 @@ package com.footballscore.activities;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.transition.Explode;
-import android.view.Window;
 
 public class BaseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 }
